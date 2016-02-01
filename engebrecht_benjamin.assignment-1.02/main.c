af1587e7f13f0a390c45d99e0f99638da405917b
textboox5 <textboox5@aim.com>
2016-01-31 23:01:05 -0600

    First method written
    It's a start

--- engebrecht_benjamin.assignment-1.02/main.c
+++ engebrecht_benjamin.assignment-1.02/main.c
@@ -6,16 +6,43 @@
 #include "gen.h"
 #include "io.h"
 
+typedef enum command {
+	read,
+	write,
+	rw
+} command_t;
+
 int main(int argc, char *argv[])
 {
+
+	command_t command;
 
 	int seed;
 
 	if (argc == 1) {
 		seed = (int)time(NULL);
+	} else if (argc == 2){
+		if (!strcmp(argv[1], "--load")) {
+			command = read;
+		} else if (!strcmp(argv[1], "--save")) {
+			command = save;
+		} else {
+			fprintf(stderr, "Bad argument\n");
+		}
+	} else if (argc == 3) {
+		if ((!strcmp(argv[1], "--load") && !strcmp(argv[2], "--save") || (!strcmp(argv[1], "--save") && !strcmp(argv[2], "--load")) {
+			command = rw;
+		} else {
+			fprintf(stderr, "Bad argument\n");			
+		}
 	} else {
-		seed = atoi(argv[1]);
+		fprintf(stderr, "Too many arguments\n");		
 	}
+
+
+	// } else {
+	// 	seed = atoi(argv[1]);
+	// }
 
 
 	//Set up random number seed
